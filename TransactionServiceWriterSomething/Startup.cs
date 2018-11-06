a458da3ff46d873056764814dba4eb58887dcbda
Shane Padellaro <shane.padellaro@airfinetworks.com>
2018-11-06 17:41:40 +0000

    Added EventWriter service

---
+++ TransactionServiceWriterSomething/Startup.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using EventStore.ClientAPI;
+using Hangfire;
+using Hangfire.AspNetCore;
+using Hangfire.Mongo;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Conventions;
+using MongoDB.Driver;
+
+namespace TransactionServiceWriterSomething
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            services.AddSingleton(f =>
+            {
+                var conn = EventStoreConnection.Create(
+                    ConnectionSettings.Create().KeepReconnecting(),
+                    ClusterSettings.Create().DiscoverClusterViaGossipSeeds().SetGossipSeedEndPoints(new[]
+                        {
+                            new IPEndPoint(IPAddress.Parse("52.151.78.42"), 2113),
+                            new IPEndPoint(IPAddress.Parse("52.151.79.84"), 2113),
+                            new IPEndPoint(IPAddress.Parse("51.140.14.214"), 2113)
+                        })
+                        .SetGossipTimeout(TimeSpan.FromMilliseconds(500)).Build(),"TransactionService");
+
+                conn.ConnectAsync().Wait();
+                return conn;
+            });
+            
+            services.AddTransient<IMongoDatabase>(s =>
+                {
+                    var conventionPack = new  ConventionPack {new CamelCaseElementNameConvention()
+                        ,new EnumRepresentationConvention(BsonType.String)};
+                    ConventionRegistry.Register("default", conventionPack, t => true);
+                    var url = new MongoUrl("mongodb://localhost:27017");
+                    var client = new MongoClient(url);
+                    return client.GetDatabase("transactions");
+                }
+            );
+            
+            
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddHangfire(g =>
+            {
+                g.UseMongoStorage("mongodb://localhost:27017", "hangfire");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+            
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            
+            app.UseHangfireServer();
+            app.UseHangfireDashboard();
+            
+            Hangfire.RecurringJob.AddOrUpdate<StatementCreator>(j=>j.Run(),Cron.Monthly(1,4));
+
+        }
+    }
+}
---
+++ TransactionServiceWriterSomething/StatementCreator.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using EventStore.ClientAPI;
+using EventStoreReadModelBenchMark.Events;
+using EventStoreReadModelBenchMark.ReadModels;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace TransactionServiceWriterSomething
+{
+    public class StatementCreator
+    {
+        private readonly IMongoDatabase _database;
+        private readonly IEventStoreConnection _conn;
+
+        public StatementCreator(IMongoDatabase database,IEventStoreConnection conn)
+        {
+            _database = database;
+            _conn = conn;
+        }
+        public void Run()
+        {
+            var result = _database.ListCollections();
+            var collections =  result.ToList();
+            var names = collections.Select(x => x["name"]?.AsString)
+                .Where(x => x.Contains("Account", StringComparison.OrdinalIgnoreCase)).ToList();
+
+            var accountBalances = new Dictionary<string, long>();
+
+            var todaysDate = DateTime.UtcNow.AddMonths(2);
+            var firstDayOfMonth = new DateTime(todaysDate.Year,todaysDate.AddMonths(-1).Month,1,23,59,59);
+            var lastDayOfMonth = firstDayOfMonth.AddMonths(1).AddDays(-1);
+            
+            foreach (var name in names)
+            {
+                var transaction = _database.GetCollection<TransactionReadModel>(name).AsQueryable()
+                    .Where(x => x.MetaData.EventCreated <= lastDayOfMonth).OrderByDescending(x => x.MetaData.EventCreated)
+                    .FirstOrDefault();
+
+                if(transaction?.AccountBalance == null || transaction.AccountBalance > -1)
+                    
+                accountBalances.Add(
+                    name.TrimEnd('t', 'r', 'a', 'n', 's', 'a', 'c', 't', 'i', 'o', 'n', 's').TrimEnd('-'),
+                    transaction?.AccountBalance == null || transaction.AccountBalance > -1 ? 0 : transaction.AccountBalance );
+            }
+
+            foreach (var account in accountBalances)
+            {
+                var statementCreatedEvent = new StatementCreatedEvent(account.Value, DateTime.UtcNow);
+                var json = JsonConvert.SerializeObject(statementCreatedEvent,
+                    new JsonSerializerSettings() {ContractResolver = new CamelCasePropertyNamesContractResolver()});
+                var jsonBytes = UTF8Encoding.ASCII.GetBytes(json);
+                var e = new EventData(Guid.NewGuid(), "statementCreated", true,
+                    jsonBytes, null);
+
+                _conn.AppendToStreamAsync(account.Key, ExpectedVersion.Any, e).Wait();
+            }
+        }
+    }
+}
